6dbe39c618adef12a9be70b274ecb23b3c508c60
Olly <75307864+ladeAdeyemi@users.noreply.github.com>
2021-08-20 12:06:49 +0100

    Added some javascript to the navigation bar to make it collapse and expand in response to a scroll event

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// VARIABLE & CONSTANT DECLARATIONS
+
+const navBar = document.querySelector('nav#navigation.flex');
+
+// EVENT LISTENERS
+
+window.addEventListener('scroll', alterPadding);
+// window.addEventListener('scroll', slideNavBar);
+
+// FUNCTIONS
+
+function alterPadding () {
+
+    if (window.scrollY >= 200) {
+
+        navBar.style.padding = 0;
+
+    } else {
+
+        navBar.style.padding = '10px';
+        navBar.style.paddingLeft = 0;
+        navBar.style.paddingRight = 0;
+
+    }
+
+}
+
+// function slideNavBar () {
+
+//     if (window.scrollY >= 500) {
+//         navBar.style.transform = 'translateY(-70px)';
+//     } 
+    
+//     else {
+//         navBar.style.transform = 'translateY(50px)';
+//     }
+// }
